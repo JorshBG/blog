663d4d9247f8f06c8a6edbfc57a36cdde1a5117e
JorshBG <jordiyair29@gmail.com>
2024-02-07 07:54:36 -0600

    fix dependencies

--- importmap.php
+++ importmap.php
@@ -28,4 +28,13 @@ return [
         'version' => '2.2.1',
         'type' => 'css',
     ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '7.3.0',
+    ],
 ];
